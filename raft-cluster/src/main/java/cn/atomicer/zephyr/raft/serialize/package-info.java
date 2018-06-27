d26117b5095016ef425202deff755020633933b2
rao-mengnan <raomengnan@gmail.com>
2018-06-27 14:25:57 +0800

    package description

---
+++ raft-cluster/src/main/java/cn/atomicer/zephyr/raft/serialize/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package include MessageDecoder and MessageEncoder which similar to
+ * {@link io.netty.handler.codec.LengthFieldPrepender}
+ * and {@link io.netty.handler.codec.LengthFieldBasedFrameDecoder}
+ *
+ * @author Rao Mengnan
+ * on 2018/5/13.
+ */
+package cn.atomicer.zephyr.raft.serialize;
